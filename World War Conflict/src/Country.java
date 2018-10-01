5ed6a27c48f8dde92bf4ec9fa74b099bb03c883c
calvin.farrell <calvin.farrell@MU-C1-12.mullen.pvt.k12.co.us>
2018-10-01 14:13:04 -0600

    intial commit

---
+++ World War Conflict/src/Country.java
@@ -0,0 +1,42 @@
+
+public class Country
+	{
+		String name;
+		int numberOfNukes;
+		int numberOfLaserSystems;
+		int population;
+		
+		public String getName()
+			{
+				return name;
+			}
+		public void setName(String name)
+			{
+				this.name = name;
+			}
+		public int getNumberOfNukes()
+			{
+				return numberOfNukes;
+			}
+		public void setNumberOfNukes(int numberOfNukes)
+			{
+				this.numberOfNukes = numberOfNukes;
+			}
+		public int getNumberOfLaserSystems()
+			{
+				return numberOfLaserSystems;
+			}
+		public void setNumberOfLaserSystems(int numberOfLaserSystems)
+			{
+				this.numberOfLaserSystems = numberOfLaserSystems;
+			}
+		public int getPopulation()
+			{
+				return population;
+			}
+		public void setPopulation(int population)
+			{
+				this.population = population;
+			}
+		
+	}
