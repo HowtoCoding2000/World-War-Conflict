3ebdc8ebe53200027633a3fe9a140460f6cb8d00
calvin.farrell <calvin.farrell@MU-C1-12.mullen.pvt.k12.co.us>
2018-10-02 15:13:55 -0600

    created game added button to launch laser. fix so laser works when button pressed

--- World War Conflict/src/NukeDefenseSystemAnimation.java
+++ World War Conflict/src/NukeDefenseSystemAnimation.java
@@ -1,5 +1,89 @@
-
-public class NukeDefenseSystemAnimation
+import java.util.Scanner;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Scanner;
+import java.awt.*;
+import java.awt.event.*;
+import javax.swing.*;
+public class NukeDefenseSystemAnimation extends Canvas
 	{
-
+		//Have a nuke move across the screen and have the user choose when to fire the laser
+		//If the laser hits the nuke you save the world other wise the world is destroyed
+		private static final long        serialVersionUID        = 1L;
+		static int x = 0;
+		static int y = 100;
+		static int x2 = 230;
+		static int y2 = 300;
+		static boolean launch = false; 
+		static Scanner userInput = new Scanner(System.in);
+		public static void main(String[] args)
+			{
+				NukeDefenseSystemAnimation canvas = new NukeDefenseSystemAnimation();
+				JFrame frame = new JFrame(); //Creates new frame
+				frame.setSize(500, 500); //Sets frame size
+				frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE); //Allows X to close the frame
+				frame.getContentPane().add(canvas).setBackground(Color.white); //Sets background color to white
+				frame.setLocationRelativeTo(null); //Sets JPanel to center of screen 
+				frame.setResizable(false); //Allows frame size to be changed
+				frame.setVisible(true);
+				frame.setLayout(new GridBagLayout());
+				JButton btnA = new JButton("LAUNCH");
+				frame.add(btnA);
+				btnA.addActionListener(new ActionListener(){
+					@Override
+					public void actionPerformed(ActionEvent arg0)
+						{
+							// TODO Auto-generated method stub
+							System.out.println("Laser Activated");
+							launch = true;
+						}
+					
+				});
+//				while(x < 480)
+//					{
+//						graphics.setColor(Color.DARK_GRAY);
+//						graphics.fillOval(x2, y2, 10, 20);
+//						if (launch)
+//							{
+//								graphics.setColor(Color.red);
+//								graphics.fillRect(x2 - 4, 0, 2, 300);
+//							}
+//						graphics.setColor(Color.white);
+//						graphics.fillRect(x - 5, y, 20, 20);
+//						graphics.setColor(Color.BLACK);
+//						graphics.fillRect(x, y, 20, 20);
+//						delay();
+//						x = x + 5;
+//					}
+			}
+		public void paint(Graphics graphics)
+			{
+				while(x < 480)
+					{
+						graphics.setColor(Color.DARK_GRAY);
+						graphics.fillOval(x2, y2, 10, 20);
+						if (launch)
+							{
+								graphics.setColor(Color.red);
+								graphics.fillRect(x2 - 4, 0, 2, 300);
+							}
+						graphics.setColor(Color.white);
+						graphics.fillRect(x - 5, y, 20, 20);
+						graphics.setColor(Color.BLACK);
+						graphics.fillRect(x, y, 20, 20);
+						delay();
+						x = x + 5;
+					}
+			}
+		public static void delay()
+			{
+				try
+        			{
+        				Thread.sleep(50);
+        			} 
+        		catch (InterruptedException e)
+        			{
+        				e.printStackTrace();
+        			}
+			}
 	}
--- World War Conflict/src/WorldWarConflictMenu.java
+++ World War Conflict/src/WorldWarConflictMenu.java
@@ -1,5 +1,12 @@
 import java.util.Scanner;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Scanner;
+import java.awt.*;
+import java.awt.event.*;
+import javax.swing.*;
 public class WorldWarConflictMenu
 	{
+		static ArrayList <Country> world = new ArrayList<Country>();
 		
 	}
